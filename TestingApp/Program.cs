8e6ef94ecda7024d753631c8dd607d6b812e865e
abelagustian123 <abelagustian1234@gmail.com>
2026-03-31 13:50:25 +0700

    "Latihan-1"

--- TestingApp/Program.cs
+++ TestingApp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+string inputNumber = Console.ReadLine();
+
+if (!int.TryParse(inputNumber, out int number))
+{
+    System.Console.WriteLine("input harus angka");
+}
+else
+{
+    for(int i = 1; i <= number; i++)
+    {
+        if((i % 3 == 0) && (i % 5 == 0))
+            System.Console.Write("foobar");
+        else if(i % 3 == 0)
+            System.Console.Write("foo, ");
+        else if(i % 5 == 0)
+            System.Console.Write("bar, ");
+        else 
+            System.Console.Write(i+", ");
+    }
+}
+
