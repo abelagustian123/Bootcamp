23f71bd33cfeb4053f32c8d2a4b1f45d547d3092
abelagustian123 <abelagustian1234@gmail.com>
2026-04-06 08:58:55 +0700

    logic exercise 2

---
+++ LogicExercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿string inputNumber = Console.ReadLine();
+
+if (!int.TryParse(inputNumber, out int number))
+    System.Console.WriteLine("input harus angka");
+else
+{
+    for(int i = 1; i <= number; i++)
+    {
+        string s = "";
+        if(i % 3 == 0)
+            s += "foo";
+        if(i % 5 == 0)
+            s += "bar";
+        if(i % 7 == 0)
+            s += "jazz";
+
+        if (string.IsNullOrEmpty(s))
+            System.Console.Write(i);
+        else
+            System.Console.Write(s);
+
+        if (i < number) System.Console.Write(",");
+    }
+}
