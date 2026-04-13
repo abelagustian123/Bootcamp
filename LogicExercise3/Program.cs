af9d5cb9b67e3b9be4d018a6605d90644e03118c
abelagustian123 <73374793+abelagustian123@users.noreply.github.com>
2026-04-13 10:06:41 +0700

    Add files via upload

---
+++ LogicExercise3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+string inputNumber = Console.ReadLine();
+
+if (!int.TryParse(inputNumber, out int number))
+    System.Console.WriteLine("input harus angka");
+else
+{
+    for(int i = 1; i <= number; i++)
+    {
+        StringBuilder? s = new StringBuilder();
+        if(i % 3 == 0)
+            s.Append("foo");
+        if(i % 4 == 0)
+            s.Append("baz");
+        if(i % 5 == 0)
+            s.Append("bar");
+        if(i % 7 == 0)
+            s.Append("jazz");
+        if(i % 9 == 0)
+            s.Append("huzz");
+
+        if (s == null || s.Length == 0)
+            System.Console.Write(i);
+        else
+            System.Console.Write(s);
+
+        if (i < number) System.Console.Write(",");
+    }
+}
